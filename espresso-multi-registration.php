b8b71678aa163f16711b9d60e43b787c16db41bb
Darren Ethier <darren@roughsmootheng.in>
2013-07-17 08:56:36 -0400

    bump version modify plugin updater init to work with new pue-client code in core [touch: 3177]

--- espresso-multi-registration.php
+++ espresso-multi-registration.php
@@ -4,7 +4,7 @@
   Plugin URI: http://eventespresso.com/
   Description: Multi Events Registration addon for Event Espresso.
 
-  Version: 1.0.4
+  Version: 1.0.5.b
 
   Author: Seth Shoultes
   Author URI: http://www.eventespresso.com
@@ -26,7 +26,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
  */
-define("ESPRESSO_MULTI_REG_VERSION", '1.0.4');
+define("ESPRESSO_MULTI_REG_VERSION", '1.0.5.b');
 
 //Update notifications
 add_action('action_hook_espresso_multiple_update_api', 'ee_multiple_load_pue_update');
@@ -39,13 +39,18 @@ function ee_multiple_load_pue_update() {
 		require(EVENT_ESPRESSO_PLUGINFULLPATH . 'class/pue/pue-client.php' );
 		$api_key = $org_options['site_license_key'];
 		$host_server_url = 'http://eventespresso.com';
-		$plugin_slug = 'espresso-multiple';
+		$plugin_slug = array(
+      'premium' => array('p' => 'espresso-multiple'),
+      'prerelease' => array('b' => 'espresso-multiple-pr')
+      );
 		$options = array(
 			'apikey' => $api_key,
 			'lang_domain' => 'event_espresso',
 			'checkPeriod' => '24',
 			'option_key' => 'site_license_key',
-      'options_page_slug' => 'event-espresso'
+      'options_page_slug' => 'event-espresso',
+      'plugin_basename' => plugin_basename(dirname(__FILE__)),
+      'use_wp_update' => FALSE, //if TRUE then you want FREE versions of the plugin to be updated from WP
 		);
 		$check_for_updates = new PluginUpdateEngineChecker($host_server_url, $plugin_slug, $options); //initiate the class and start the plugin update engine!
 	}
